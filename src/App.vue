c2d062df649cb06437331aeceed400427a4815c9
ertugrulkok <101091726+ertugrulkok@users.noreply.github.com>
2022-08-22 19:59:04 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+<div>
+ <AppHeading v-for=" person in persons"
+ :key="person.id"
+ :id="person.id"
+ :name="person.name"
+ :age="person.age"
+ :height="person.height"
+ :nationality="person.nationality"
+ :gender="person.gender"
+ ></AppHeading>
+</div>
+
+
+
+ 
+</template>
+
+<script>
+import AppHeading from "./components/appHeading.vue";
+
+export default {
+  components: {
+  
+    AppHeading
+},
+
+data(){
+  return{
+    persons:[{
+        id:"pes",
+        name:"Markus Merk",
+        age: "20",
+        height:"1,73",
+        nationality:"German",
+        gender:"male"
+        }
+    
+    ]
+  }
+}
+};
+</script>
---
+++ src/components/appHeading.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header>Die Person {{name}} is {{age}} jahre alt und kommt von {{nationality}}. Die person ist {{gender}} und {{height}} gßoss</header>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"appHeading",
+
+
+  props:{
+    id:String,
+    height:String,
+    age:String,
+    name:String,
+    nationality:String,
+    gender:String,
+  }
+}
+</script>
+
+
+<style>
+header {
+  background-color: rebeccapurple;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  position: absolute;
+  top: 0;
+  left: 0%;
+  border-bottom: 2px solid black;
+}
+</style>
